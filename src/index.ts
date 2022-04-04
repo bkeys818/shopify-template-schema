2f91a852ab904248a6b2d207fef215d66dba7dd2
Ben Keys <bkeys818@gmail.com>
2022-04-04 11:50:33 -0400

    🚚 Rename index.ts to template.ts

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,4 @@
-import { createSectionSchema, type Section } from './section'
-import type { JSONSchema7 } from 'json-schema'
-export type { Section }
-export type { Block } from './block'
-export type { Setting } from './settings'
-
-export function createTemplateSchema(
-    sections: Record<string, Section>
-): JSONSchema7 {
-    return {
-        type: 'object',
-        properties: {
-            name: { type: 'string' },
-            layout: {
-                anyOf: [{ const: false }, { type: 'string' }],
-                default: 'theme.liquid',
-            },
-            wrapper: { type: 'string' },
-            sections: {
-                type: 'object',
-                additionalProperties:
-                    Object.keys(sections).length > 0
-                        ? {
-                              anyOf: Object.entries(sections).map(
-                                  ([fileName, schema]) =>
-                                      createSectionSchema(fileName, schema)
-                              ),
-                          }
-                        : false,
-                maxProperties: 20,
-            },
-            order: {
-                type: 'array',
-                items: { type: 'string' },
-                maxItems: 20,
-                uniqueItems: true,
-            },
-        },
-        required: ['sections', 'order'],
-        additionalProperties: false,
-    }
-}
+export { createTemplateSchema } from './template'
+export { createSectionSchema, Section } from './section'
+export { createBlockSchema, Block } from './block'
+export { createSettingSchema, Setting } from './settings'
---
+++ src/template.ts
@@ -0,0 +1,39 @@
+import { createSectionSchema, type Section } from './section'
+import type { JSONSchema7 } from 'json-schema'
+
+export function createTemplateSchema(
+    sections: Record<string, Section>
+): JSONSchema7 {
+    return {
+        type: 'object',
+        properties: {
+            name: { type: 'string' },
+            layout: {
+                anyOf: [{ const: false }, { type: 'string' }],
+                default: 'theme.liquid',
+            },
+            wrapper: { type: 'string' },
+            sections: {
+                type: 'object',
+                additionalProperties:
+                    Object.keys(sections).length > 0
+                        ? {
+                              anyOf: Object.entries(sections).map(
+                                  ([fileName, schema]) =>
+                                      createSectionSchema(fileName, schema)
+                              ),
+                          }
+                        : false,
+                maxProperties: 20,
+            },
+            order: {
+                type: 'array',
+                items: { type: 'string' },
+                maxItems: 20,
+                uniqueItems: true,
+            },
+        },
+        required: ['sections', 'order'],
+        additionalProperties: false,
+    }
+}
